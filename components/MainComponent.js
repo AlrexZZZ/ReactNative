c7d9950201c26b567b682334e3d25fc3649a888f
AlexZ <alex.zxm@foxmail.com>
2020-07-30 18:53:25 -0500

    navigator 1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,32 +1,51 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes';
 import Dishdetail from './DishdetailComponent';
-import { View } from 'react-native';
+import { DISHES } from '../shared/dishes';
+import { View, Platform } from 'react-native';
+import 'react-native-gesture-handler';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
 
+const MenuNavigatorStack = createStackNavigator({
+    Menu: { screen: Menu },
+    Dishdetail: { screen: Dishdetail }
+},
+    {
+        initialRouteName: 'Menu',
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: "#512DA8"
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: "#fff"
+            }
+        }
+    }
+);
+
+const MenuNavigator = createAppContainer(MenuNavigatorStack);
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES,
-      selectedDish: null
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES,
+            selectedDish: null
+        };
+    }
+    onDishSelect(dishId) {
+        this.setState({ selectedDish: dishId })
+    }
+    render() {
 
-  onDishSelect(dishId) {
-    this.setState({selectedDish: dishId})
+        return (
+            <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+                <MenuNavigator />
+            </View>
+        );
+    }
 }
 
-  render() {
- 
-    return (
-      <View style={{flex:1}}>
-      <Menu dishes={this.state.dishes} onPress={(dishId) => this.onDishSelect(dishId)} />
-      <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-  </View>
-    );
-  }
-}
-  
 export default Main;
