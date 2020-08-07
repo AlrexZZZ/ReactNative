af73ba8dcd3fbcd03aac2cfc75b7c4de059fbcf8
AlexZ <alex.zxm@foxmail.com>
2020-08-07 13:00:43 -0500

    gesture to comment

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -32,6 +32,13 @@ function RenderDish(props) {
       return false;
   };
 
+  const recognizeComment = ({ moveX, moveY, dx, dy }) => {
+    if (dx>200) 
+      return true;
+    else
+      return false;
+  };
+
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: (e, gestureState) => {
         return true;
@@ -41,7 +48,7 @@ function RenderDish(props) {
         .then(endState => console.log(endState.finished ? 'finished' : 'cancelled'));
     },
     onPanResponderEnd: (e, gestureState) => {
-      if (recognizeDrag(gestureState))
+      if (recognizeDrag(gestureState)){
         Alert.alert(
           'Add to Favorites',
           'Are you sure you wish to add ' + dish.name + ' to your favorites?',
@@ -58,7 +65,10 @@ function RenderDish(props) {
             },
           ],
           { cancelable: false }
-        )
+        )}
+        else if (recognizeComment(e,gestureState)){
+        props.toggleCommentModal() }
+        else
       return true;
     }
   });
